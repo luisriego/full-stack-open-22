03b9a1b55312df6124e98e8b42ca3e1cedec944c
luisriego <luisriego@hotmail.com>
2022-06-25 20:29:11 -0300

    part2 steo

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -3,50 +3,59 @@ import ReactDOM from 'react-dom/client';
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1,
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2,
       },
       {
         name: 'State of a component',
-        exercises: 14
-      }
-    ]
+        exercises: 14,
+        id: 3,
+      },
+    ],
   }
 
+  return <Course course={course} />
+}
+
+const Course = ({course}) => {
   return (
     <div>
       <Header course={course} />
       <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      {/* <Total parts={course.parts} /> */}
     </div>
   )
 }
 
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>
+const Header = ({course}) => {
+  return <h1>{course.name}</h1>
 }
 
-const Content = (props) => {
+const Content = ({parts}) => {
   return (
     <div>
-      <Part parts={props.parts[0]} />
-      <Part parts={props.parts[1]} />
-      <Part parts={props.parts[2]} />
+      {parts.map(part => (
+          <Part key={part.id} part={part} />
+      ))}
     </div>
   )
 }
 
-const Part = (props) => {
+const Part = ({part}) => {
+  console.log(part)
   return (
     <p>
-      {props.parts.name} {props.parts.exercises}
+      {part.name} {part.exercises}
     </p>
   )
 }
