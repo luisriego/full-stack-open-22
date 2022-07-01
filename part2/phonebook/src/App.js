495fce54ec3091b53a7c2f7e650856b3c0a38a52
luisriego <luisriego@hotmail.com>
2022-07-01 20:03:34 -0300

    step2 exercise 2.7 ended

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const personObj = [{ name: newName }]
+    console.log(persons)
+    if (persons.some(item => item.name === newName)) {
+      alert(newName + ' is already added to phonebook')
+    }
+    setPersons(persons.concat(personObj))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
+    </div>
+  )
+}
+
+export default App
